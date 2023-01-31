e8a5180fe12e5db00a1d25080fec09b7e0c3f98e
Ajnos Cz <tomashrebicek11@gmail.com>
2023-01-31 22:28:55 +0100

    new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-    <header>
+    <header className='flex content-between items-center pt-5 pb-5 pl-[10%] pr-[10%]'>
       <img src="/logo.png" alt="A-icon"/>
       <nav>
         <ul>
