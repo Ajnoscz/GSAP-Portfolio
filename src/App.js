01279459a67f0808d3c7f4d73bbc4862180fe403
Ajnoscz <59149310+Ajnoscz@users.noreply.github.com>
2023-02-06 14:26:19 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import './header.css'
+import gsap from 'gsap';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+  
+   gsap.fromTo("#text", {y: 100, opacity: 0}, {y:0, opacity:1, stagger: 0.5});
+  })
+  
   return (
     <>
     <header className='navbar'>
       <img className='image' src="/logo.png" alt="A-icon"/>
       <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/">Socials</a></li>
+        <ul className='links'>
+          <li id='text'><a href="/">Home</a></li>
+          <li id='text'><a href="/">Socials</a></li>
         </ul>
       </nav>     
     </header>
